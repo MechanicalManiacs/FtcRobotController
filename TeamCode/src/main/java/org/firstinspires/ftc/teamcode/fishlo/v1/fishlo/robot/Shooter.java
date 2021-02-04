9e92ce5047cd23053088cf9f4dabe380b0cdeda6
KeshavVarad <kvarad2020@gmail.com>
2021-02-03 20:19:57 -0500

    Fixed shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/robot/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/robot/Shooter.java
@@ -104,7 +104,7 @@ public class Shooter extends SubSystem {
         mecanumDrive.update();
         Pose2d drivePose = mecanumDrive.getPoseEstimate();
         Pose2d goalPose = goalMap.get(target).getPosition();
-        double height = (goalMap.get(target).getHeight()-8.5) * 0.0254;
+        double height = (goalMap.get(target).getHeight()-9.5) * 0.0254;
 
         double goalDistance = Math.sqrt(Math.pow(drivePose.getX() - goalPose.getX(), 2) +
                 Math.pow(drivePose.getY() - goalPose.getY(), 2)) * 0.0254;
